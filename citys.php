6aafdfc0daeadda690189f5354281997d88e5fcf
kobi171 <kobi171@gmail.com>
2017-01-07 13:14:21 +0200

    Add files via upload

---
+++ citys.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p {
+    font-family: "Comic Sans MS", cursive, sans-serif;
+	text-align: center;
+	font-size: 250%;
+}
+h2 {
+	position: center;
+}
+</style>
+</head>
+<body>
+<p>Choose city</p>
+<form>
+
+<h2>
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+position: center;
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+<input style="width: 300px; 
+padding: 30px; 
+margin: 10px;
+cursor: pointer; 
+box-shadow: 6px 6px 5px; #999; 
+-webkit-box-shadow: 6px 6px 5px #999; 
+-moz-box-shadow: 6px 6px 5px #999; 
+font-weight: bold; 
+background: #8fbc8f; 
+color: #000; 
+border-radius: 
+10px; border: 1px solid #999; 
+font-size: 150%;" 
+type="button" 
+value="city 2" 
+onclick="window.location.href='http://www.hyperlinkcode.com/button-links.php'" />
+
+</h2>
+
+
+
+
+
+</form>
+
+
+</body>
+</html>
+
+
+
+
