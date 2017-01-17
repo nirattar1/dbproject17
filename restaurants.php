28b7a3dd3e9a789371b45777c6796c86d03d6821
kobi171 <kobi171@gmail.com>
2017-01-17 22:35:21 +0200

    Add files via upload

--- restaurants.php
+++ restaurants.php
@@ -30,9 +30,28 @@ tr:nth-child(6n+5) { background: hsl(200, 99%, 50%); }
 <body>
 <p>Your Best Match:</p>
 <?php
-$badget= $_GET["badget"];
-$category= $_GET["category"];
 $city= $_GET["city"];
+$category= $_GET["category"];
+$badget= $_GET["badget"];
+$story= $_GET["story"];
+$from_hour= $_GET["from_hour"];
+$to_hour= $_GET["to_hour"];
+$day= $_GET["day"];
+$from;
+$to;
+
+if ($badget==1){
+	$from=0;
+	$to=10;
+}
+else if ($badget==2){
+	$from=10;
+	$to=50;
+}
+else if ($badget==3){
+	$from=50;
+	$to=10000;
+}
 
 function connect(){
 	$servername = "mysqlsrv.cs.tau.ac.il";
@@ -50,9 +69,62 @@ function connect(){
 	return $conn;
 }
 $conn = connect(); 
-$sql = "select Restaurant.name, Restaurant.checkinsCount , Restaurant.id from Restaurant order by Restaurant.checkinsCount desc limit 15";
-$result = $conn->query($sql);
-?>
+if($story==3) {
+$sql1 = "select name, restaurant_avg
+from (
+select restaurant_table.name as name, avg(restaurant_table.section_avg) as restaurant_avg
+from (
+select section_table.name, section_table.id, section_table.section_name , 
+avg(section_table.price) as section_avg 
+from (
+select Restaurant.name, Restaurant.id , Dish.section_name , Dish.name as name1  , Dish.price
+from Restaurant , City , Category , Dish
+where  City.name='$city' and Category.name='$category' and City.id=Restaurant.city_id and 
+Restaurant.categories=Category.id and Dish.restaurant_id=Restaurant.id
+) as section_table
+group by section_table.section_name
+) as restaurant_table
+group by restaurant_table.name desc
+) as avg_by_budget
+where 0< restaurant_avg and restaurant_avg <10
+limit 15";
+}
+
+else if($story==1){
+	$str="";
+	if ($from_hour != "-1") {
+		$str=$str." and o.open_hour<=\"".$from_hour.":00\"";
+		if ($to_hour == "-1") {
+			$str=$str." and o.close_hour>=\"".$from_hour.":00\"";
+		}
+	}
+	if ($to_hour != "-1") {
+		$str=$str." and o.close_hour>=\"".$to_hour.":00\"";
+		if ($from_hour == "-1") {
+			$str=$str." and o.open_hour<=\"".$to_hour.":00\"";
+		}
+	}
+	if($day!="-1") {
+		$str=$str." and o.day=".$day;
+	}
+	$sql1 = "select r.name, r.checkinsCount , r.id
+					FROM Restaurant r
+					WHERE EXISTS 
+  					(SELECT * FROM City c, OpenHours o 
+   					  where c.id=r.city_id and r.id=o.restaurant_id $str)
+   					  order by r.checkinsCount
+   					  desc limit 15";
+	}
+
+
+$result = $conn->query($sql1);
+
+if ( $result->num_rows > 0 ){ 
+	
+	
+?>	
+
+
 
 <table>
 	<tr>
@@ -66,12 +138,13 @@ $result = $conn->query($sql);
 			<td> <?php echo $i+1; ?> </td>
 			<?php  $row = $result->fetch_assoc(); ?>
 			<td><a href="rest.php?rest=<?php echo $row["name"]; ?>&id=<?php echo $row["id"]; ?>&city=<?php echo $city; ?>"> <?php echo $row["name"]; ?> </a></td>
-			<td> <?php echo $row["checkinsCount"]; ?> </td>
+			<td> <?php echo $row["restaurant_avg"]; ?> </td>
 	</tr>
 		<?php 
 	} ?>	
 
 </table>
 <?php $conn->close(); ?>
+<?php } ?>
 </body>
 </html>
